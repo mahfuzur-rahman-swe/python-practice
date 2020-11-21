a620ee5cd71e5097817410388cd1c18fd968f10d
mahfuz00741 <mahfuzur35-2207@diu.edu.bd>
2020-11-21 23:05:21 +0600

    delete

--- class 8/dictionary.py
+++ class 8/dictionary.py
@@ -16,7 +16,22 @@ info = {"name": "Mahfuzur Rahman", "Id": "172-35-2207", "Department": "Software 
 
 info["name"] = "Tipu Sultan"
 print(info["name"])
-'''
+
 info = {"name": "Mahfuzur Rahman", "Id": "172-35-2207", "Department": "Software Engineering"}
 print("Element count: ", len(info))
 
+#Delete dictionary
+info = {"name": "Mahfuzur Rahman", "Id": "172-35-2207", "Department": "Software Engineering"}
+del info["name"]
+print(info)
+#or delete key by method
+info.__delitem__("Id")
+print("After Id delete: ", info)
+
+# Clear all element
+info = {"name": "Mahfuzur Rahman", "Id": "172-35-2207", "Department": "Software Engineering"}
+info.clear()
+print(info)
+#or
+del info
+'''
