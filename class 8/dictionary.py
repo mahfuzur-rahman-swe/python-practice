0f774353e94d84f53d9cdd018a80c8f1ca8aeda0
mahfuz00741 <mahfuzur35-2207@diu.edu.bd>
2020-11-21 22:56:13 +0600

    element count

--- class 8/dictionary.py
+++ class 8/dictionary.py
@@ -9,10 +9,14 @@ print(mydict)
 info = {"name": "Mahfuzur Rahman", "Id": "172-35-2207", "Department": "Software Engineering"}
 
 print("Name: ", info["name"])
-'''
+
 # update dictionary
 
 info = {"name": "Mahfuzur Rahman", "Id": "172-35-2207", "Department": "Software Engineering"}
 
 info["name"] = "Tipu Sultan"
 print(info["name"])
+'''
+info = {"name": "Mahfuzur Rahman", "Id": "172-35-2207", "Department": "Software Engineering"}
+print("Element count: ", len(info))
+
