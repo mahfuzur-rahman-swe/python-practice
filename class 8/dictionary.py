53e2756052dae01ed95f7b6105001d0805785f57
mahfuz00741 <mahfuzur35-2207@diu.edu.bd>
2020-11-21 22:54:19 +0600

    Update dictionary

--- class 8/dictionary.py
+++ class 8/dictionary.py
@@ -4,8 +4,15 @@ print(info["name"])
 mydict = dict()
 
 mydict["01"] = "John"
-print(mydict)'''
+print(mydict)
 
 info = {"name": "Mahfuzur Rahman", "Id": "172-35-2207", "Department": "Software Engineering"}
 
 print("Name: ", info["name"])
+'''
+# update dictionary
+
+info = {"name": "Mahfuzur Rahman", "Id": "172-35-2207", "Department": "Software Engineering"}
+
+info["name"] = "Tipu Sultan"
+print(info["name"])
