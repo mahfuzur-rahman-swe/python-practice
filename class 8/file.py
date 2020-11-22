df37cfdc5f505d9a3854db41403ae32d187802a2
mahfuz00741 <mahfuzur35-2207@diu.edu.bd>
2020-11-22 12:55:00 +0600

    all work about file

---
+++ class 8/file.py
@@ -0,0 +1,33 @@
+file = open("Demo.txt", "w")
+file.write("This is first line\n")
+file.write("this is second line\n")
+file.write("this is third line\n")
+file.write("this is fourth line")
+file.close()
+file = open("Demo.txt", "r")
+print(file.readline(2))
+
+'''file = open("Demo.txt", "r")
+print(file.read())
+file.close()
+file = open("Demo.txt", "r")
+print("\n",file.read(10))
+file.close()
+
+#If we wanted to return only the 2nd line in the file, we would use this:
+
+file = open("Demo.txt", "r")
+print("\n", file.readline(2))
+
+file = open("demo2.txt","w")
+file.write("Hello World\n")
+file.write("This is our new text file\n")
+file.write("and this is another line.\n")
+file.write("Why? Because we can do it this easily.")
+file.close()
+
+file = open("Demo2.txt", "r")
+for x in file:
+    print(x)'''
+
+
